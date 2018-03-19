898b23a7a56abf672d42916eba1d8c2283cc74aa
aws1718jmlr <joslucrod@alum.us.es>
2018-03-19 17:05:06 +0000

    Initial program's structure

---
+++ index.js
@@ -0,0 +1,73 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var BASE_API_PATH = "/api/v1";
+var dbFileName = __dirname + "/contacts.json";
+
+console.log("Starting server...");
+
+var app = express();
+app.use(bodyParser.json());
+
+var initialContacts = [
+    { "name": "peter", "phone": 12345 },
+    { "name": "john", "phone": 6789 }
+    /*
+    Direccion	Avda. Reina Mercedes, s/n
+    Localidad	Sevilla
+    CP	41012
+    Telefono	95.455.65.00/02/04
+    Fax	95.455.65.34
+    Email	malala@us.es
+    http	http://www.arquitectura.us.es/
+
+    */
+];
+
+
+app.get(BASE_API_PATH + "/universities", (req, res) => {
+   
+});
+
+app.post(BASE_API_PATH + "/universities", (req, res) => {
+  
+});
+
+app.put(BASE_API_PATH + "/universities", (req, res) => {
+    // Forbidden
+    console.log(Date()+" - PUT /universities");
+
+    res.sendStatus(405);
+});
+
+app.delete(BASE_API_PATH + "/universities", (req, res) => {
+    
+});
+
+
+app.post(BASE_API_PATH + "/universities/:name", (req, res) => {
+    // Forbidden
+    console.log(Date()+" - POST /universities");
+
+    res.sendStatus(405);
+});
+
+app.get(BASE_API_PATH + "/universities/:name", (req, res) => {
+ 
+});
+
+
+app.delete(BASE_API_PATH + "/universities/:name", (req, res) => {
+  
+});
+app.delete(BASE_API_PATH + "/universities/:name", (req, res) => {
+  
+});
+
+app.put(BASE_API_PATH + "/universities/:name", (req, res) => {
+   
+});
+
+app.get("/", (req, res) => {
+    res.send("<html><body><h1>My server</h1></body></html>");
+});
