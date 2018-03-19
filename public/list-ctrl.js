894773e773efeb987ea9190a33987f0d754f5f73
scarjo11 <sezzle-wave@live.fr>
2018-03-19 18:17:43 +0000

    POST method an Refresh call in list-ctrl.js

--- public/list-ctrl.js
+++ public/list-ctrl.js
@@ -2,6 +2,16 @@ angular
     .module("UniversityListApp")
     .controller("ListCtrl", function($scope,$http) {
         
+        $scope.addUniversity = function (){
+            
+            $http
+                .post("/api/v1/universities/:name", $scope.newUniversity)
+                .then(function (){
+                    refresh();  
+                });
+        
+        }
+        
         refresh();
         
     });
