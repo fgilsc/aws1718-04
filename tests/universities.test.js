c7f2fc4a3e28b14747e2f8ae37d9e5cff12ea61d
JoseM717 <jmpkd7@gmail.com>
2018-05-11 17:07:00 +0000

    Added support to MongoDB instead of Nedb

--- tests/universities.test.js
+++ tests/universities.test.js
@@ -5,32 +5,40 @@ chai.use(require('chai-things'));
 var expect = chai.expect;
 var universities = require('../universities.js');
 
-describe('Universities', function() {
+describe('Universities', function(){
     beforeEach(function(done) {
-        universities.removeAll(function(err) {
+        
+        universities.connectDb((err) => {
             if (err) {
                 return done(err);
             }
             
-            universities.add([{
-                name: "Universidad de Cádiz", 
-                address: "C/ Ancha 16",
-                city:"Cádiz",
-                ZipCode:"11001",
-                phone:"956019101",
-                fax:"956015049",
-                mail:"secretaria.general@uca.es",
-                web:"http://www.uca.es/" 
-            }, {
-                name: "Universidad de Salamanca", 
-                address: "Cardenal Plá y Deniel, 22",
-                city:"Salamanca",
-                ZipCode:"37008",
-                phone:"923294400",
-                fax:"923294693",
-                mail:"informacion@usal.es",
-                web:"http://www.usal.es/"
-            }], done);
+            universities.removeAll(function(err) {
+                if (err) {
+                    return done(err);
+                }
+                
+                
+                universities.add([{
+                    name: "Universidad de Cádiz", 
+                    address: "C/ Ancha 16",
+                    city:"Cádiz",
+                    ZipCode:"11001",
+                    phone:"956019101",
+                    fax:"956015049",
+                    mail:"secretaria.general@uca.es",
+                    web:"http://www.uca.es/" 
+                }, {
+                    name: "Universidad de Salamanca", 
+                    address: "Cardenal Plá y Deniel, 22",
+                    city:"Salamanca",
+                    ZipCode:"37008",
+                    phone:"923294400",
+                    fax:"923294693",
+                    mail:"informacion@usal.es",
+                    web:"http://www.usal.es/"
+                }], done);
+            });
         });
     });
     
--- universities.js
+++ universities.js
@@ -1,18 +1,27 @@
 'use strict';
 
-var path = require('path');
-var DataStore = require('nedb');
-var dbFileName = path.join(__dirname, 'universities.json');
-
-var db = new DataStore({
-    filename : dbFileName,
-    autoload : true
-});
+var MongoClient = require('mongodb').MongoClient;
+var db;
 
 var Universities = function () {};
 
+Universities.prototype.connectDb = function(callback){
+    MongoClient.connect(process.env.MONGODB_URL, function(err, database) {
+        if(err) {
+            callback(err);
+        }
+        
+        db = database.db('aws').collection('universities');
+        
+        callback(err, db);
+    });
+};
+
+
+
+
 Universities.prototype.allUniversities = function(callback){
-    return db.find({},callback);
+    return db.find({}).toArray(callback);
 };
 
 Universities.prototype.add = function(university,callback){
@@ -24,7 +33,7 @@ Universities.prototype.removeAll = function(callback){
 };
 
 Universities.prototype.get = function(name,callback){
-    return db.find({name:name},callback);
+    return db.find({name:name}).toArray(callback);
 };
 
 Universities.prototype.remove = function(name,callback){
@@ -36,3 +45,4 @@ Universities.prototype.update = function(name, updatedUniversity,callback){
 };
 
 module.exports = new Universities();
+
