f717fd4b1536f1493f5c77bbc51bc92c9985cd4f
JoseM717 <jmpkd7@gmail.com>
2018-05-11 17:52:47 +0000

    commit

---
+++ aws1718-04/README.md
@@ -0,0 +1,10 @@
+# aws1718-04
+Talleres AWS 2017-18.
+
+Franciso Gil Sánchez-Carrasco
+José Manuel Lucas Rodríguez
+Joscar Nzembi
+
+Enlace a la presentación: https://docs.google.com/presentation/d/e/2PACX-1vREJgm6cZgSshC79AOJlF3f688d9enOUCwUo_vQuXzUGGObyI8ldXIv6zLwPQtanAgji5E10Ddq4sid/pub?start=false&loop=false&delayms=5000
+Enlace a pruebas postman: https://documenter.getpostman.com/view/4054387/document/RVu1HWhD
+Enlace a Travis: https://travis-ci.org/fgilsc/aws1718-04
---
+++ aws1718-04/index.js
@@ -0,0 +1,197 @@
+'use strict';
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var DataStore = require("nedb");
+var path = require('path');
+
+
+var BASE_API_PATH = "/api/v1";
+var dbFileName = __dirname + "/universities.json";
+
+var port = (process.env.PORT || 16778);
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+console.log("Starting API server...");
+var initialUniversities = [
+
+    { "name": "Universidad de Sevilla", 
+      "address": "C/ S. Fernando, 4",
+      "city":"Sevilla",
+      "ZipCode":41004,
+      "phone":954551000,
+      "fax":954557000,
+      "mail":"serviasuntosg@us.es",
+      "web":"http://www.us.es/" 
+    },
+    { "name": "Universidad Complutense de Madrid", 
+      "address": "Avda. de Séneca, 2 Ciudad Universitaria",
+      "city":"Madrid",
+      "ZipCode":28040,
+      "phone": 923294500,
+      "fax":914520400,
+      "mail":"infocom@ucm.es",
+      "web":"https://www.ucm.es/"
+    },
+    { "name": "Universidad de Oviedo", 
+      "address": " Calle San Francisco, 1",
+      "city":"Oviedo",
+      "ZipCode":33003,
+      "phone":985103000,
+      "fax":958103933,
+      "mail":"coie@uniovi.es",
+      "web":"http://www.uniovi.es/" 
+    }
+];
+
+var db = new DataStore({
+    filename: dbFileName,
+    autoload: true
+});
+
+db.find({},(err,universities)=>{
+    if(err){
+        console.error("Error accesing DB");
+        process.exit(1);
+    }else{
+        if(universities.length == 0){
+            console.log("Empty DB, initializaing data...");
+            db.insert(initialUniversities);
+        }else{
+            console.log("Loaded DB with "+universities.length+" universities.");
+        }
+           
+    }
+});
+
+app.get(BASE_API_PATH + "/universities", (req, res) => {
+    // Obtain all contacts
+    console.log(Date()+" - GET /universities");
+    
+    db.find({},(err,universities)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+        }else{
+            res.send(universities.map((university)=>{
+                delete university._id;
+                return university;
+            }));
+        }
+    });
+});
+
+app.post(BASE_API_PATH + "/universities", (req, res) => {
+    console.log(Date()+" - POST /universities");
+    var university = req.body;
+    db.insert(university);
+    res.sendStatus(201);
+});
+
+app.put(BASE_API_PATH + "/universities", (req, res) => {
+    // Forbidden
+    console.log(Date()+" - PUT /universities");
+
+    res.sendStatus(405);
+});
+
+app.delete(BASE_API_PATH + "/universities", (req, res) => {
+   // Remove all universities
+   console.log(Date()+" - DELETE /universities");
+
+   db.remove({}, { multi: true });
+   
+   res.sendStatus(200); 
+});
+
+
+app.post(BASE_API_PATH + "/universities/:name", (req, res) => {
+    // Forbidden
+    console.log(Date()+" - POST /universities");
+
+    res.sendStatus(405);
+});
+
+app.get(BASE_API_PATH + "/universities/:name", (req, res) => {
+  // Get a single university
+    var name = req.params.name;
+    console.log(Date()+" - GET /universities/"+name);
+
+    db.find({"name": name},(err,universities)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+        }else{
+            if(universities.length>1){
+                console.warn("Incosistent DB: duplicated name");
+            }
+            res.send(universities.map((university)=>{
+                delete university._id;
+                 console.log("Uni: "+university.name);
+                return university;
+            })[0]);
+        }
+    });
+});
+
+
+app.delete(BASE_API_PATH + "/universities/:name", (req, res) => {
+  // Delete a single university
+  var name = req.params.name;
+  console.log(Date()+" - DELETE /universities/"+name);
+
+  db.remove({"name": name},{},(err,numRemoved)=>{
+      if(err){
+          console.error("Error accesing DB");
+          res.sendStatus(500);
+      }else{
+          if(numRemoved>1){
+              console.warn("Incosistent DB: duplicated name");
+          }else if(numRemoved == 0) {
+              res.sendStatus(404);
+          } else {
+              res.sendStatus(200);
+          }
+      }
+  });
+});
+
+app.put(BASE_API_PATH + "/universities/:name", (req, res) => {
+    var name = req.params.name;
+    
+    var updatedUniversity = req.body;
+    console.log(Date()+" - PUT /universities/"+name);
+    
+    
+    if(name != updatedUniversity.name){
+        res.sendStatus(409);
+        return;
+    }
+
+    
+    db.update({"name": name},updatedUniversity,(err,numUpdated)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+        }else{
+            if(numUpdated>1){
+                console.warn("Incosistent DB: duplicated name");
+            }else if(numUpdated == 0) {
+                res.sendStatus(404);
+            } else {
+                res.sendStatus(200);
+            }
+        }
+    });
+});
+
+app.get("/", (req, res) => {
+    res.send("<html><body><h1>My server</h1></body></html>");
+});
+
+app.listen(port, () => {
+    console.log("Server with GUI up and running!!");
+});
